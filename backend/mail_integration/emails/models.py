99581db5876c2b1c58716a9cc151c86077b1a662
Makarena-211 <mnfomenkov@gmail.com>
2024-07-02 16:46:59 +0300

    created schemas in pg

---
+++ backend/mail_integration/emails/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class EmailAccount(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.email
+
+class EmailMessage(models.Model):
+    email_account = models.ForeignKey(EmailAccount, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=255)
+    date_sent = models.DateTimeField()
+    date_recieved = models.DateTimeField()
+    body = models.TextField()
+    attachments = models.JSONField()
+
+    def __str__(self):
+        return self.topic
